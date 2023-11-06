5cfe0416350d0af24e967bfc722a641643360ac0
Aditya Lohumi <lohumiaditya15@gmail.com>
2023-11-06 11:12:23 +0530

    Create API for deleting Issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -17,3 +17,12 @@ export async function PATCH(request: NextRequest, {params} : {params: {id : stri
     return NextResponse.json(updatedIssue);
     
 }
+
+export async function DELETE(request: NextRequest, {params} : {params: {id : string}}) {
+    const issue = await prisma.issue.findUnique({where: {id: parseInt(params.id)}});
+    if(!issue) return NextResponse.json("Invalid Issue", {status: 404});
+
+    await prisma.issue.delete({where: {id: issue.id}});
+
+    return NextResponse.json({});
+}
