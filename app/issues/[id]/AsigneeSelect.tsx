9c7aee0c27ddde613d1f5f5d716dde65d31423c1
Aditya Lohumi <lohumiaditya15@gmail.com>
2023-11-07 11:49:40 +0530

    Populated AsigneeSelect with backend data

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -1,15 +1,28 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AsigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const { data: users } = await axios.get<User[]>("/api/users");
+      setUsers(users);
+    };
+    getUserData();
+  });
   return (
     <Select.Root>
       <Select.Trigger placeholder="Apply..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Aditya Lohumi</Select.Item>
+          {users.map((user) => (
+            <Select.Item value={user.id}>{user.name}</Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
