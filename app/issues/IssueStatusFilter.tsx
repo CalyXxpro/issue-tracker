9bde02176986343b2c04457d39601c80b75ce238
Aditya Lohumi <lohumiaditya15@gmail.com>
2023-11-08 16:32:09 +0530

    Fix Filtering Bugs

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Select } from "@radix-ui/themes";
 import { Status } from "@prisma/client";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const IssueStatusFilter = () => {
   const statuses: { label: string; status?: Status }[] = [
@@ -12,11 +12,17 @@ const IssueStatusFilter = () => {
     { label: "Closed", status: "CLOSED" },
   ];
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || "none"}
       onValueChange={(status) => {
-        const query = status === "none" ? "" : `?status=${status}`;
+        const params = new URLSearchParams();
+        if (status !== "none") params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        const query = params.size ? "?" + params.toString() : "";
         router.push("/issues" + query);
       }}
     >
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -12,7 +12,6 @@ const IssuesPage = async ({
 }: {
   searchParams: { status: Status; orderBy: keyof Issue };
 }) => {
-  console.log(searchParams.orderBy);
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
@@ -24,8 +23,12 @@ const IssuesPage = async ({
     className: string | undefined;
   }[] = [
     { label: "Issue", value: "title", className: undefined },
-    { label: "Status", value: "status", className: "block md:hidden" },
-    { label: "Created At", value: "createdAt", className: "block md:hidden" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    {
+      label: "Created At",
+      value: "createdAt",
+      className: "hidden md:table-cell",
+    },
   ];
 
   const orderBy = columns
@@ -48,7 +51,10 @@ const IssuesPage = async ({
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{ query: { ...searchParams, orderBy: column.value } }}
                 >
